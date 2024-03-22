4fabd30b57714cac908ce39fe3f638da34666fb2
sangvorennlabs <155945208+sangvorennlabs@users.noreply.github.com>
2024-03-22 16:29:01 +0700

    Update app_deploy_sample_result.py

--- app_deploy_sample_result.py
+++ app_deploy_sample_result.py
@@ -12,9 +12,15 @@ with st.sidebar:
 if input_password in ['rennlabs123***']:
     json_data_claude = [json.load(open(f'sample_result_claude_agent_2.1/{x}', 'r')) for x in os.listdir('sample_result_claude_agent_2.1')]
     json_data_openai = [json.load(open(f'sample_result_openai_agent/{x}', 'r')) for x in os.listdir('sample_result_openai_agent')]
+    json_input = json.load(open('input.json', 'r'))
+    
     idx = st.number_input('Sample number', 1, len(json_data_openai)) - 1
+
+    company_name = os.listdir('sample_result_claude_agent_2.1')[idx].split('.')[0]
+    company_url = [x for x in json_input if x["Company name"]==company_name][0]["Company website"] if company_name!="Renn Global" else "https://www.rennglobal.com/"
+
     st.write('# Input')
-    st.write(f'## Company name:\n{json_data_claude[idx]["firm_name"]}')
+    st.write(f'## Company name:\n{company_name}')
     st.write(f'## Company website:\n{json_data_openai[idx]["website_url"]}')
     col1, col2 = st.columns([1,1])
     with col1:
